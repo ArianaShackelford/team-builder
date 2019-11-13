63636f1d1dc5ee242c33d640d5c79bc19349102e
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-13 18:54:19 -0500

    added onChange to imputs

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -7,15 +7,19 @@ const MemberForm = props => {
         setMember({...member, [event.target.name]: event.target.value})
     };
 
-    
+    const submitForm = event => {
+        event.preventDefault()
+        props.addNewMember(member)
+        setMember({name: '', email: '', role: ''})
+    }
     return(
         <form>
             <label htmlFor='name'>Name: </label>
-            <input id='name' type='text' name='name'/>
+            <input id='name' type='text' name='name' onChange={handleChanges}/>
             <label htmlFor='email'>Email: </label>
-            <input id='email' type='email' name='email'/>
+            <input id='email' type='email' name='email' onChange={handleChanges}/>
             <label htmlFor='role'>Role: </label>
-            <select id='role'>
+            <select id='role' onChange={handleChanges}>
                 <option value=''>-- Please choose a role --</option>
                 <option value='jellyfishTrainer'>Jellyfish Trainer</option>
                 <option value='emuWhisperer'>Emu Whisperer</option>
