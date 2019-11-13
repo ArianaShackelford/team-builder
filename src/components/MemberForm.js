b98ea8a9ee9ece0de4da8ea270fed159574b1a48
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-13 18:11:55 -0500

    created basic form and useState

---
+++ src/components/MemberForm.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const MemberForm = props => {
+    const [member, setMember] = useState({name: '', email: '', role: ''})
+
+    return(
+        <form>
+            <label htmlFor='name'>Name: </label>
+            <input id='name' type='text' name='name'/>
+            <label htmlFor='email'>Email: </label>
+            <input id='email' type='text' name='email'/>
+            <label htmlFor='role'>Role: </label>
+            <input id='role' type='dropdown' name='role'/>
+        </form>
+    )
+}
+
+export default MemberForm;
